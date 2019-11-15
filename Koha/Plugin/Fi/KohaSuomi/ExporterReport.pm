0e8d996498aaed80447c32937322ece74349b724
Johanna Raisa <johanna.raisa@gmail.com>
2019-11-15 13:01:02 +0200

    Add plugin structure

---
+++ Koha/Plugin/Fi/KohaSuomi/ExporterReport.pm
@@ -0,0 +1,134 @@
+package Koha::Plugin::Fi::KohaSuomi::ExporterReport;
+
+## It's good practice to use Modern::Perl
+use Modern::Perl;
+
+## Required for all plugins
+use base qw(Koha::Plugins::Base);
+
+## We will also need to include any Koha libraries we want to access
+use C4::Context;
+use utf8;
+
+## Here we set our plugin version
+our $VERSION = "1.0";
+
+## Here is our metadata, some keys are required, some are optional
+our $metadata = {
+    name            => 'Siirtoraportti',
+    author          => 'Johanna Räisä',
+    date_authored   => '2019-11-14',
+    date_updated    => "2019-11-14",
+    minimum_version => '17.05.00.000',
+    maximum_version => undef,
+    version         => $VERSION,
+    description     => 'Siirtoraportti tietueiden valutukseen.',
+};
+
+## This is the minimum code required for a plugin's 'new' method
+## More can be added, but none should be removed
+sub new {
+    my ( $class, $args ) = @_;
+
+    ## We need to add our metadata here so our base class can access it
+    $args->{'metadata'} = $metadata;
+    $args->{'metadata'}->{'class'} = $class;
+
+    ## Here, we call the 'new' method for our base class
+    ## This runs some additional magic and checking
+    ## and returns our actual $self
+    my $self = $class->SUPER::new($args);
+
+    return $self;
+}
+
+## The existance of a 'report' subroutine means the plugin is capable
+## of running a report. This example report can output a list of patrons
+## either as HTML or as a CSV file. Technically, you could put all your code
+## in the report method, but that would be a really poor way to write code
+## for all but the simplest reports
+
+sub report {
+    my ( $self, $args ) = @_;
+    my $cgi = $self->{'cgi'};
+
+    $self->report_view();
+}
+
+## If your tool is complicated enough to needs it's own setting/configuration
+## you will want to add a 'configure' method to your plugin like so.
+## Here I am throwing all the logic into the 'configure' method, but it could
+## be split up like the 'report' method is.
+sub configure {
+    my ( $self, $args ) = @_;
+    my $cgi = $self->{'cgi'};
+
+    unless ( $cgi->param('save') ) {
+        my $template = $self->get_template({ file => 'configure.tt' });
+
+        ## Grab the values we already have for our settings, if any exist
+        $template->param(
+            baseendpoint => $self->retrieve_data('baseendpoint'),
+            apikey => $self->retrieve_data('apikey'),
+            environment => $self->retrieve_data('environment')
+        );
+
+        print $cgi->header();
+        print $template->output();
+    }
+    else {
+        $self->store_data(
+            {
+                baseendpoint          => $cgi->param('baseendpoint'),
+                apikey                => $cgi->param('apikey'),
+                environment           => $cgi->param('environment')
+            }
+        );
+        $self->go_home();
+    }
+}
+
+## This is the 'install' method. Any database tables or other setup that should
+## be done when the plugin if first installed should be executed in this method.
+## The installation method should always return true if the installation succeeded
+## or false if it failed.
+sub install() {
+    my ( $self, $args ) = @_;
+
+    return 1;
+}
+
+## This is the 'upgrade' method. It will be triggered when a newer version of a
+## plugin is installed over an existing older version of a plugin
+sub upgrade {
+    my ( $self, $args ) = @_;
+
+    return 1;
+}
+
+## This method will be run just before the plugin files are deleted
+## when a plugin is uninstalled. It is good practice to clean up
+## after ourselves!
+sub uninstall() {
+    my ( $self, $args ) = @_;
+
+    return 1;
+}
+
+sub report_view {
+    my ( $self, $args ) = @_;
+    my $cgi = $self->{'cgi'};
+
+    my $template = $self->get_template({ file => 'report.tt' });
+
+    $template->param(
+        baseendpoint => $self->retrieve_data('baseendpoint'),
+        apikey => Digest::SHA::hmac_sha256_hex($self->retrieve_data('apikey')),
+        environment => $self->retrieve_data('environment')
+    );
+
+    print $cgi->header();
+    print $template->output();
+}
+
+1;
---
+++ Koha/Plugin/Fi/KohaSuomi/ExporterReport/js/viewApp.js
@@ -0,0 +1,159 @@
+new Vue({
+    el: '#viewApp',
+    created() {
+        this.fetchExports();	
+    },
+    data: {
+        results: [],
+        errors: [],
+        status: "pending",
+        isActive: false,
+        page: 1,
+        limit: 50,
+        pages: 1,
+        startCount: 1,
+        endPage: 11,
+        lastPage: 0
+    },
+    methods: {
+        fetchExports() {
+            this.errors = [];
+            axios.get(baseendpoint+'biblio/report/'+environment, {
+                headers: { Authorization: apitoken },
+                params: {status: this.status, page: this.page, limit: this.limit}
+            }).then(response => {
+                this.results = response.data.results;
+                this.pages = Math.ceil(response.data.count/this.limit);
+                if (this.pages == 0) {
+                    this.pages = 1;
+                }
+                this.activate();
+            }).catch(error => {
+                this.errors.push(error.response.data.error);
+            });
+        },
+        changeStatus(status, event) {
+            event.preventDefault();
+            $(".nav-link").removeClass("active");
+            $(event.target).addClass("active");
+            this.results = [];
+            this.status = status;
+            this.page = 1;
+            this.fetchExports();
+        },
+        changePage(e, page) {
+            e.preventDefault();
+            if (page < 1) {
+                page = 1;
+            }
+            if (page > this.pages) {
+                page = this.pages;
+            }
+            this.page = page;
+            if (this.page == this.endPage) {
+                this.startCount = this.page;
+                this.endPage = this.endPage+10;
+                this.lastPage = this.page;
+            }
+            if (this.page < this.lastPage) {
+                this.startCount = this.page-10;
+                this.endPage = this.lastPage;
+                this.lastPage = this.lastPage-10;
+            }
+            this.fetchExports();
+        },
+        activate() {
+            $(".page-link").removeClass("bg-primary text-white");
+            $("[data-current="+this.page+"]").addClass("bg-primary text-white");
+        },
+        pageHide(page) {
+            if (this.pages > 5) {
+                if (this.endPage <= page && this.startCount < page) {
+                    return true;
+                }
+                if (this.endPage >= page && this.startCount > page) {
+                    return true;
+                }
+            }
+
+            
+        }
+    }
+});
+Vue.component('result-list', {
+  template: "#list-items",
+  methods: {
+    getRecord(e, id) {
+        e.preventDefault();
+        this.errors = [];
+        axios.get(baseendpoint+'biblio/record/'+id, {headers: { Authorization: apitoken }}
+        ).then(response => {
+            $("#modalWrapper").find("#recordModal").remove();
+            var html = $('<div id="recordModal" class="modal fade" role="dialog">\
+                        <div class="modal-dialog modal-lg">\
+                            <div class="modal-content">\
+                                <div class="modal-header">\
+                                    <h5 class="modal-title">Tietue</h5>\
+                                    <button type="button" class="close" data-dismiss="modal" aria-label="Close">\
+                                        <span aria-hidden="true">&times;</span>\
+                                    </button>\
+                                </div>\
+                                <div id="recordWrapper" class="modal-body">\
+                                </div>\
+                                <div class="modal-footer">\
+                                    <button type="button" class="btn btn-default" data-dismiss="modal">Sulje</button>\
+                                </div>\
+                            </div>\
+                        </div>\
+                    </div>');
+            $('#modalWrapper').append(html);
+            var source = parseRecord(response.data);
+            $('#recordModal').find('#recordWrapper').append($('<div class="container">'+source+'</div>'));
+            $('#recordModal').modal('toggle');
+        }).catch(error => {
+            this.errors.push(error.response.data.error);
+        });
+    }
+  },
+  filters: {
+        moment: function (date) {
+            return moment(date).locale("fi").format('D.M.Y H:mm:ss');
+        },
+        type: function (type) {
+            if (type == "add") {
+                return "Uusi"
+            }
+            if (type == "update") {
+                return "Muokkaus"
+            }
+        }
+    },
+  props: ['result']
+});
+
+parseRecord = function (record) {
+    var html = '<div>';
+    html += '<li class="row"> <div class="col-xs-3 mr-2>';
+    html += '<b>000</b></div><div class="col-xs-9">'+record.leader+'</li>';
+    record.fields.forEach(function(v,i,a){
+        if ($.isNumeric(v.tag)) {
+            html += '<li class="row"><div class="col-xs-3 mr-2">';
+        } else {
+            html += '<li class="row hidden"><div class="col-xs-3  mr-2">';
+        }
+        html += '<b>'+v.tag;
+        if (v.ind1) {html += ' '+v.ind1}
+        if (v.ind2) {html += ' '+v.ind2}
+        html += '</b></div><div class="col-xs-9">';
+        if (v.subfields) {
+            v.subfields.forEach(function(v,i,a){
+                html += '<b>_'+v.code+'</b>'+v.value+'<br/>';
+            });
+        } else {
+            html += v.value;
+        }
+        html += '</div></li>';
+    });
+    html += '</div>';
+    return html;
+}
