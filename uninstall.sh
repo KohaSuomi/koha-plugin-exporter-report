c48ecf7e448f775b85234f10aaa6a14b2e6704f4
Johanna Raisa <johanna.raisa@gmail.com>
2024-07-15 12:14:50 +0300

    Add remove manage_plugins to uninstall

--- uninstall.sh
+++ uninstall.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 
 kohaplugindir="$(grep -Po '(?<=<pluginsdir>).*?(?=</pluginsdir>)' $KOHA_CONF)"
+
 echo "Removing $kohaplugindir/Koha/Plugin/Fi/KohaSuomi/ExporterReport"
 rm -r $kohaplugindir/Koha/Plugin/Fi/KohaSuomi/ExporterReport
 rm $kohaplugindir/Koha/Plugin/Fi/KohaSuomi/ExporterReport.pm
+
+utilitydir="/home/koha/koha-suomi-utility"
+perl $utilitydir/misc/manage_plugins.pl --remove Koha::Plugin::Fi::KohaSuomi::ExporterReport
