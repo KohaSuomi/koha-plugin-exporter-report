50998662a853e08578a5cdbbbcc87a2ae6eb1045
Johanna Raisa <johanna.raisa@gmail.com>
2024-07-15 11:06:36 +0300

    Add echo information

--- uninstall.sh
+++ uninstall.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 kohaplugindir="$(grep -Po '(?<=<pluginsdir>).*?(?=</pluginsdir>)' $KOHA_CONF)"
-
+echo "Removing $kohaplugindir/Koha/Plugin/Fi/KohaSuomi/ExporterReport"
 rm -r $kohaplugindir/Koha/Plugin/Fi/KohaSuomi/ExporterReport
 rm $kohaplugindir/Koha/Plugin/Fi/KohaSuomi/ExporterReport.pm
